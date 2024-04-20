47c1cca7f5c15207945e84df749d3ae24dce06f2
Matvei <matvesch@protonmail.com>
2024-04-20 18:50:56 +0300

    only

--- src/components/Stops/Stops.tsx
+++ src/components/Stops/Stops.tsx
@@ -7,24 +7,27 @@ export default function Stops() {
     <div className={styles.stops}>
       {stopsOptions.map((option) => (
         <div key={option} className={styles.outerContainer}>
-          <label className={styles.container}>
-            <input type="checkbox" />
-            <span className={styles.checkmark}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="4"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-check"
-              >
-                <path d="M20 6 9 17l-5-5" />
-              </svg>
-            </span>
-          </label>
-          <div>{option}</div>
+          <div style={{ display: 'flex', gap: '5px' }}>
+            <label className={styles.container}>
+              <input type="checkbox" />
+              <span className={styles.checkmark}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-check"
+                >
+                  <path d="M20 6 9 17l-5-5" />
+                </svg>
+              </span>
+            </label>
+            <div>{option}</div>
+          </div>
+          <button className={styles.only}>только</button>
         </div>
       ))}
     </div>
