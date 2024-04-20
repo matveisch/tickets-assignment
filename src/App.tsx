b24b04e45c00bc9708fabc2ce3076c6f75c6123e
Matvei <matvesch@protonmail.com>
2024-04-20 21:24:52 +0300

    filtering

--- src/App.tsx
+++ src/App.tsx
@@ -32,12 +32,32 @@ export async function getData<T>(url: string): Promise<T | undefined> {
 
 function App() {
   const [tickets, setTickets] = useState<TicketType[] | undefined>();
+  const [filteredTickets, setFilteredTickets] = useState<TicketType[] | undefined>(tickets);
   const [currency, setCurrency] = useState<string>('RUB');
   const [stops, setStops] = useState<boolean[]>([true, false, false, false, false]);
 
   useEffect(() => {
     getData<TicketType[]>('Tickets.json').then((data) => setTickets(data));
   }, []);
+
+  useEffect(() => {
+    function filterFlightsByStops(flights: TicketType[], userStopSelection: boolean[]) {
+      // Combine stops and no-stops selections
+      const stopsToInclude = userStopSelection.slice(1, 5); // Get elements at indices 2, 3, and 4
+
+      return flights.filter((flight) => {
+        const flightStops = flight.stops;
+        // Check if number of stops in flight matches any selected option (0, 1, 2, or 3)
+        return stopsToInclude[flightStops];
+      });
+    }
+
+    if (!stops[0]) {
+      tickets && setFilteredTickets(filterFlightsByStops(tickets, stops));
+    } else {
+      setFilteredTickets(tickets);
+    }
+  }, [stops, tickets]);
 
   return (
     <CurrencyContext.Provider value={{ currency, setCurrency }}>
@@ -46,9 +66,9 @@ function App() {
           <img className={styles.logo} src="/plane-logo.png" alt="plane-logo" />
           <div className={styles.mainContainer}>
             <Filter />
-            {tickets && (
+            {filteredTickets && (
               <div className={styles.ticketsContainer}>
-                {tickets.map((ticket, index) => (
+                {filteredTickets.map((ticket, index) => (
                   <Ticket ticket={ticket} key={`ticket-${index}`} />
                 ))}
               </div>
